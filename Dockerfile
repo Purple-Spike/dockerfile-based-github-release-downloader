93b9feedcf7073e8cbcaae51cf300aaf079a20a3
Al Rodriguez <ProgrammerAl@Outlook.com>
2022-10-24 15:43:48 -0400

    Got the script and Dockerfile just about there. Need to integrate with DigitalOcean's build system, so that will probably bring some changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 
 FROM mcr.microsoft.com/powershell:latest AS build
+ARG ACCESS_TOKEN
+ARG RELEASE_TAG_ID
+ARG ASSET_NAME
+ARG GITHUB_REPO_URI
 
 WORKDIR /src
 
 COPY ["script.ps1", "script.ps1"]
 
-RUN pwsh ./script.ps1
+RUN pwsh ./script.ps1 -githubAccessToken "${ACCESS_TOKEN}" -releaseTagId "${RELEASE_TAG_ID}" -assetName "${ASSET_NAME}" -githubUri "${GITHUB_REPO_URI}"
+
--- script.ps1
+++ script.ps1
@@ -1,28 +1,9 @@
-# uri https://github.com/Purple-Spike/public-web-app
-# releaseId 3200335278
-# asset PublicApi.zip
-# access token 
-
-# pwsh ./script.ps1 -githubAccessToken "" -releaseTagId 3200335278 -assetName "PublicApi.zip" -githubUri "https://github.com/Purple-Spike/public-web-app"
-
 param (
     [string]$githubAccessToken,
     [string]$githubUri,
     [Int64]$releaseTagId,
     [string]$assetName
 )
-
-# Install-Module -Name PowerShellForGitHub
-
-$githubAccessTokenSecure = ($githubAccessToken | ConvertTo-SecureString -AsPlainText -Force)
-
-$githubCreds = New-Object System.Management.Automation.PSCredential "username is ignored", $githubAccessTokenSecure
-Set-GitHubAuthentication -Credential $githubCreds
-
-#clear these out now that they're no longer needed
-$githubAccessTokenSecure = $null
-$githubCreds = $null
-$githubAccessToken = $null 
 
 if ($releaseTagId -lt 1) {
     throw 'Release Id is required'
@@ -33,6 +14,21 @@ if ([string]::IsNullOrWhiteSpace($githubUri)) {
 if ([string]::IsNullOrWhiteSpace($assetName)) {
     throw 'Asset Name is required'
 }
+if ([string]::IsNullOrWhiteSpace($githubAccessToken)) {
+    throw 'GitHub Access Token is required'
+}
+
+Install-Module -Name PowerShellForGitHub -AcceptLicense -Force
+
+$githubAccessTokenSecure = ($githubAccessToken | ConvertTo-SecureString -AsPlainText -Force)
+
+$githubCreds = New-Object System.Management.Automation.PSCredential "username is ignored", $githubAccessTokenSecure
+Set-GitHubAuthentication -Credential $githubCreds
+
+#clear these out now that they're no longer needed
+$githubAccessTokenSecure = $null
+$githubCreds = $null
+$githubAccessToken = $null 
 
 $downloadedDir = "./downloads"
 $extractedDir = "./extracted"
